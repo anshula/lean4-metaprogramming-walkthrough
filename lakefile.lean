93e76ad8ac7d511d5fd9a6a99f5d04aaaafa621f
Anshula Gandhi <anshula@users.noreply.github.com>
2024-02-06 15:53:57 +0000

    cleanup

--- lakefile.lean
+++ lakefile.lean
@@ -11,14 +11,6 @@ require verso from git "https://github.com/leanprover/verso" @ "main"
 
 -- require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "master"
 
-@[default_target]
-lean_exe «leanmetaprogrammingwalkthrough» where
-  root := `LeanMetaprogrammingWalkthrough
-  -- Enables the use of the Lean interpreter by the executable (e.g.,
-  -- `runFrontend`) at the expense of increased binary size on Linux.
-  -- Remove this line if you do not need such functionality.
-  supportInterpreter := true
-
 -- A demo site that shows how to generate websites with Verso
 lean_lib DemoSite where
   srcDir := "website"
