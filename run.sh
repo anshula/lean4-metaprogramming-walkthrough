664a0a3f0c13b3fab40cb354314d572440416d38
Anshula Gandhi <anshula@users.noreply.github.com>
2024-02-07 12:12:26 +0000

    removing authors

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-lake build
+# lake build
+rm -rf _out/walkthroughsite
 lake exe walkthrough --output _out/walkthroughsite
 python3 -m http.server 8800 --directory _out/walkthroughsite
--- website/Walkthrough/Blog/Introduction.lean
+++ website/Walkthrough/Blog/Introduction.lean
@@ -3,11 +3,6 @@ open Verso Genre Blog
 
 #doc (Post) "Introduction" =>
 
-%%%
-authors := ["Fictional Author"]
-date := {year := 2024, month := 1, day := 15}
-%%%
-
 This is a tutorial on metaprogramming, or equivalently, writing tactics to help prove math theorems, in Lean 4.
 That is, instead of focusing on writing a _formalized proof_ (programming), we focus on writing a _program that writes a formalized proof_ (metaprogramming).
 
@@ -16,3 +11,5 @@ That is, instead of focusing on writing a _formalized proof_ (programming), we f
 - Here is a [short "cheet sheet"](http://google.com) containing helper functions you might find useful when metaprogramming in the future.
 
 # Looking Ahead
+
+hi
