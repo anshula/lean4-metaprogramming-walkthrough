c1cc6436ac0afc99786a7780610a7e29f67c057a
Anshula Gandhi <anshula@users.noreply.github.com>
2024-02-07 12:15:45 +0000

    introduction has code running

--- website/Walkthrough/Blog/Introduction.lean
+++ website/Walkthrough/Blog/Introduction.lean
@@ -12,4 +12,24 @@ That is, instead of focusing on writing a _formalized proof_ (programming), we f
 
 # Looking Ahead
 
-hi
+By the end of the tutorial, you will have built an "auto-generalization" Lean tactic that takes an unnecessarily-weak theorem and turns it into a stronger theorem with an analogous proof (using an algorithm from the paper [Generalization in Type Theory Based Proof Assistants](http://cedric.cnam.fr/~pons/PAPERS/types00.pdf) by Oliver Pons).
+
+```leanInit demo
+-- This block initializes a Lean context
+```
+
+For example, given the theorem that √2 is irrational….
+```lean demo
+def sqrt : Nat → Nat := sorry
+def Irrational : Nat → Prop := sorry
+theorem sqrt_two_irrational :
+    Irrational (sqrt 2) := sorry
+```
+
+…this tactic will notice the proof never uses any properties of “2” besides that it is prime, and so it can generalize to the theorem that √p is irrational when p is prime.
+
+```lean demo
+def Nat.Prime : Nat → Prop := sorry
+theorem sqrt_prime_irrational :
+    ∀ (p : Nat), Nat.Prime p → Irrational (sqrt p) := sorry
+```
