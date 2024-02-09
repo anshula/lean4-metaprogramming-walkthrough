a9fcb917ebfd1826983db33523f41d1da3afffa6
Anshula Gandhi <anshula@users.noreply.github.com>
2024-02-09 15:21:27 +0000

    added fonts

--- website/Walkthrough/Blog/ReadingAndChangingTheGoal.lean
+++ website/Walkthrough/Blog/ReadingAndChangingTheGoal.lean
@@ -19,7 +19,7 @@ elab "do_nothing" : tactic => do
   return
 ```
 
-Please feel free to **paste in these bits of code into your editor**, creating one big Lean file as we go! 
+Please feel free to **paste in these bits of code into your editor**, creating one big Lean file as we go!
 
 
 We’ll see that, indeed, this tactic changes nothing about the proof state (hover over the end-of-line bubbles to see the goal state):
@@ -31,7 +31,7 @@ example : True := by
 
 # Reading the context
 
-Now, let’s create a tactic `print_goal` that reads what the current goal is. 
+Now, let’s create a tactic `print_goal` that reads what the current goal is.
 ```lean readingAndChangingTheGoal
 elab "print_goal" : tactic => do
   let goal ← getMainGoal
@@ -42,39 +42,11 @@ Let’s test the tactic:
 
 ```lean readingAndChangingTheGoal
 example : 1+1=2 := by
-  print_goal -- 1+1=2 
+  print_goal -- 1+1=2
   trivial
 ```
 
 And we get what we expect.
-
-++
-/-
-# Reading the context another way
-
-The above “goal” is really just a variable referencing the goal. It has type  `MVarId`, as can be seen by hovering over the constant named `goal` in the code block above.
-
-To get the actual goal, you’d call `getType` on the variable (which gives you something of type `Expr`), and that will give you the actual goal expression:
-
-```lean readingAndChangingTheGoal
-elab "print_goal_type" : tactic => do
-  let goal ← getMainGoal
-  let goalType ← goal.getType
-  logInfo goalType
-```
-
-Using `print_goal` and `print_goal_type` both print the same thing though…
-
-```lean readingAndChangingTheGoal
-example : True := by
-  print_goal_type -- True
-  trivial
-```
-
-…since when we were printing the goal variable, `logInfo` had some magic inside it that knew we actually wanted to print the _relevant goal information_ stored in the variable.
-
-The `getMainTarget` command accomplishes the same thing as getting the goal and then getting its type.
-++
 
 # Modify the context
 
@@ -86,12 +58,21 @@ theorem ctrp {P Q : Prop} : (¬ Q → ¬ P) → (P → Q) := by
   intro h
   rwa [not_imp_not] at h
 ```
-We can test it out.
+
+Now, if we ever want to use this theorem, we can type `apply ctrp`.
 ```lean readingAndChangingTheGoal
 example {P : Prop} : P → True := by
-  apply ctrp; simp
+  apply ctrp
+  simp
 ```
 
-And it does what we expect.
+But since the line `apply ctrp` is so cumbersome to write out, lets wrap it up into a one-word tactic called `contrapos`.
 
-Now we want to take the line `apply ctrp`, and, because it is so cumbersome to write out, wrap it up into a one-word tactic called `contrapos`.
+
+## Writing a tactic --  what doesn’t work
+
+Now, we’ve been using `elab (name) : tactic => ...` to create tactics.
+But `elab` is not very convenient to use if we are just planning on conglomerating a bunch of already-existing Lean tactics.
+
+
+That’s because there are a bunch of low-level configuration options you need to send to `apply` if you’re going to call it from within a tactic, and that’s a bit of a pain.
