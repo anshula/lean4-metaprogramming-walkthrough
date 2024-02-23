6d8779e5a17709116b31cb5b8b12c76f3b35e804
ART0 <18333981+0Art0@users.noreply.github.com>
2024-02-23 12:55:02 +0000

    First draft on adding/modifying goals

--- website/Walkthrough/Blog/AddingGoalsAndHypotheses.lean
+++ website/Walkthrough/Blog/AddingGoalsAndHypotheses.lean
@@ -5,28 +5,65 @@ open Verso Genre Blog
 #doc (Page) "Adding goals and hypotheses" =>
 
 ```leanInit AGH
+open Lean Meta Elab Tactic
 ```
 
 By the end of this section, you'll create a version of the `apply` tactic.
 
-This is the tactic that is routinely used in Lean for backwards reasoning, i.e., reasoning backwards from the target.
+We will begin by writing tactics to access and modify the goals in the tactic state.
 
-For example, suppose the goal is to prove that `2 ^ 3` is not a prime number. The library contains the result `Nat.Prime.not_prime_pow`, which says that if `x` and `n` are natural numbers and `2 ≤ n`, then `x ^ n` is not a prime number.
+# Getting the goals
 
-A mathematician would say "to show that `2 ^ 3` is not prime, it is sufficient to show that `2 ≤ 3`, by the result `Nat.Prime.not_prime_pow`." (except maybe for the last bit); the `apply` tactic is a way to formally mirror this style of reasoning in Lean.
+We've seen before that `getMainGoal` gives us the details of the current goal. The `getUnsolvedGoals` command gives us *all* the active goals in the tactic state.
+
+Here is a tactic that lets us test this out.
 
 ```lean AGH
-#check Nat.Prime.not_prime_pow
+elab "print_goals" : tactic => do
+  let goals ← getUnsolvedGoals
+  for goal in goals do
+    logInfo goal
 
-example : ¬ Nat.Prime (2 ^ 3) := by
-  apply Nat.Prime.not_prime_pow
-  -- the goal is now to show that `2 ≤ 3`
-  decide
+example : 1 + 1 = 2 ∧ 2 + 2 = 4 := by
+  print_goals
+  constructor
+  print_goals
+  all_goals rfl
 ```
 
-We will begin by writing a tactic that creates an additional goal in the tactic state. Along the way, we will take a deeper look at how the tactic state in Lean works.
+# Modifying the goals
+
+The list of goals can also be modified with the `setGoals` command.
+
+Here is an implementation of a `rotate_goals` tactic that reorders the goals to push the main goal to the end.
+
+```AGH
+elab "rotate_goals" : tactic => do
+  let goals ← getUnsolvedGoals
+  setGoals goals.rotateLeft
+
+example : 1 + 1 = 2 ∧ 2 + 2 = 4 := by
+  constructor
+  rotate_goals -- the goals are now in a different order
+  all_goals rfl
+```
+
+Now what happens if the user decides to use `setGoals` to just delete the list of active goals?
+
+``AGH error := true
+elab "clear_goals" : tactic => do
+  setGoals []
+
+example : 1 + 1 = 2 ∧ 2 + 2 = 4 := by
+  constructor
+  clear_goals -- there are no goals here
+```
+
+Doing this indeed clears all the goals in the tactic state, but a low-level kernel error now pops up near the start of the declaration. So Lean can't be tricked into accepting an incomplete proof, and the responsibility of making sure no active goals get dropped is on the meta-programmer.
 
 # Adding a new goal
+
+We will now look at modifying the tactic state beyond just manipulating the list of goals.
 
 *Creating a goal* in Lean is really *creating a metavariable* (a variable whose value a.k.a proof isn’t known yet).
 *Proving a goal* in Lean is *assigning a value a.k.a proof to that metavariable*.
@@ -67,6 +104,18 @@ elab "create_reflexivity_goal" : tactic => do
 example : 1 + 2 = 3 := by
   create_reflexivity_goal; swap
   simp; simp
+```
+
+We can also modify the syntax of the tactic to include the type of the new goal as a parameter.
+
+```lean AGH
+elab "create_goal_of" t:term : tactic => do
+  let e ← Term.elabTerm t none
+  createGoal e
+
+example : 1 + 2 = 3 := by
+  create_goal_of (Nat.Prime 2)
+  simp; exact Nat.prime_two
 ```
 
 # Adding a new hypothesis
