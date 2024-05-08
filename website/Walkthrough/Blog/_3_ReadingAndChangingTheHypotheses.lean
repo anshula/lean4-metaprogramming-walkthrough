3e0401079dc01d8bd37f9ced149507364b98fd23
Anshula Gandhi <anshula@users.noreply.github.com>
2024-05-08 11:11:11 +0100

    Update _3_ReadingAndChangingTheHypotheses.lean

--- website/Walkthrough/Blog/_3_ReadingAndChangingTheHypotheses.lean
+++ website/Walkthrough/Blog/_3_ReadingAndChangingTheHypotheses.lean
@@ -10,12 +10,10 @@ open Verso Genre Blog
 
 ```lean readingAndChangingTheHypotheses show:=false
 set_option linter.unusedVariables false
-
 open Lean Elab Tactic
-
 ```
 
-All of the exercises here are going to build up to writing an `assumption` tactic — one that looks at all the hypotheses, and if any matches the goal, successfully proves it using that hypothesis.
+All of the exercises in this section are going to build up to writing an `assumption` tactic — one that looks at all the hypotheses, and if any matches the goal, successfully proves it using that hypothesis.
 
 
 
@@ -118,8 +116,8 @@ We can do this as long as we move out the side-effect-causing tasks into a funct
 
 ```lean readingAndChangingTheHypotheses
 def printHypotheses : TacticM Unit := do
-  let goal ← getMainGoal  -- the dynamically generated hypotheses are associated with this particular goal
-  for ldecl in (← goal.getDecl).lctx do
+  let goal ← getMainGoal
+  for ldecl in ← getLCtx do
     if ldecl.isImplementationDetail then continue
     let hypName := ldecl.userName
     let hypType := ldecl.type
@@ -144,3 +142,60 @@ def printHypotheses : TacticM Unit := do
 In general, if we ever write a tactic that doesn’t return anything (and perhaps, just prints something to the screen), it will have the `Unit` return type.
 
 The `Unit` serves the purpose that `void` does in other programming languages — it tells us the function isn’t going to return anything interesting.
+
+# MetaM vs TacticM
+
+Our `printHypotheses` function worked when we had it return a `TacticM`.
+
+But if we change it to use `MetaM`, it fails.
+```
+def printHypotheses' : MetaM Unit := do
+  let goal ← getMainGoal
+  for ldecl in (← goal.getDecl).lctx do
+    if ldecl.isImplementationDetail then continue
+    let hypName := ldecl.userName
+    let hypType := ldecl.type
+    logInfo m!"Name: '{hypName}'  Type: '{hypType}'"
+```
+That’s because working within `TacticM` gives us access to the current goals — if we try to access the goals outside `TacticM`, we get an error.
+
+In fact, `TacticM` has access to everything `MetaM` has access too, plus goals.
+
+# So why ever use MetaM?
+
+So if `TacticM` is just `MetaM` but more, why would we ever use `MetaM`?
+
+The following code works if you use `MetaM`…
+```lean readingAndChangingTheHypotheses
+/-- Check the context for a theorem named `riemannHypothesis`. -/
+def lookIntoEnvironment  : MetaM Unit := do
+  let env ← getEnv
+  dbg_trace (env.contains `riemannHypothesis)
+
+#eval lookIntoEnvironment
+```
+
+… but fails if you use `TacticM`.
+
+```
+/-- Check the context for a theorem named `riemannHypothesis`. -/
+def lookIntoEnvironment'  : TacticM Unit := do
+  let env ← getEnv
+  dbg_trace (env.contains `riemannHypothesis)
+
+#eval lookIntoEnvironment'
+```
+
+What’s going on?
+
+If we ever want to check how our functions work using `#eval` statements, they need to implement the `MetaEval` class — which allows output to be easily evaluated.  `MetaM` implements this class,  but `TacticM` doesn’t.
+
+So, we ideally want to return something of type `TacticM` only when we really need it.
+
+In summary, here are the perks of each:
+- `MetaM` lets you access most of the local context (including declared _meta_variables, therefore the name `MetaM`).
+- `TacticM` lets you access anything in `MetaM`, and also the current goals you have in the theorem the _tactic_ is being used in (therefore the name `TacticM`)..
+
+And here are the downsides:
+- We can’t use a `MetaM` function to peer into a goal — `MetaM` doesn’t allow us access to a list of goals.
+- We can’t call a `TacticM` function from `#eval`.
