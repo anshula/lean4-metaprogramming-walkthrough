cd8ab518d65ee8d0d1b0cfe811023abf4019a275
Anshula Gandhi <anshula@users.noreply.github.com>
2024-07-16 17:22:48 +0100

    first bit of subexpr

--- website/Walkthrough/Blog/_7_ManipulatingSubexpressions.lean
+++ website/Walkthrough/Blog/_7_ManipulatingSubexpressions.lean
@@ -4,4 +4,30 @@ open Verso Genre Blog
 
 #doc (Page) "Manipulating Subexpressions" =>
 
-Coming soon.
+Now that we have an expression, let’s try to find some of its subexpressions.
+
+In particular, to create an autogeneralization tactic, we need to look thorugh all lemmas used in a proof, and check which ones use the term we want to generalize.  This requires checking whether a subexpression occurs in a larger expression.
+
+# Getting and filtering lists of subexpressions
+
+We can first recursively collect a list of all the subexpressions in an expression `e`:
+
+```leanInit manipSub
+```
+
+```lean manipSub
+open Lean
+
+def getSubexpressionsIn (e : Expr) : List Expr :=
+  let rec getSubexpressionsInRec (e : Expr) (acc : List Expr) : List Expr :=
+    match e with
+    | Expr.forallE _ d b _   => [e] ++ (getSubexpressionsInRec d acc) ++ (getSubexpressionsInRec b acc)
+    | Expr.lam _ d b _       => [e] ++ (getSubexpressionsInRec d acc) ++ (getSubexpressionsInRec b acc)
+    | Expr.app f a           => [e] ++ (getSubexpressionsInRec f acc) ++ (getSubexpressionsInRec a acc)
+    | Expr.mdata _ b         => [e] ++ (getSubexpressionsInRec b acc)
+    | Expr.proj _ _ b        => [e] ++ (getSubexpressionsInRec b acc)
+    | e                      => [e] ++ acc
+  let subexprs := getSubexpressionsInRec e [];
+  let subexprs := subexprs.filter $ fun subexpr => !subexpr.hasLooseBVars -- remove the ones that will cause errors when parsing
+  subexprs
+```
